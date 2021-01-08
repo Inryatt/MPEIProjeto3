8d4c41df5221d2783957c4eb5fc842d9bfc342ad
Inryatt <inrymail@gmail.com>
2021-01-08 11:13:30 +0000

    Update main.m

--- main.m
+++ main.m
@@ -45,14 +45,27 @@ while choice ~= 4
             show_movies(userid,u,movies);
         
         case 3
+        
+            % Maneira sem o minHash
             name = input("Write a string: ", 's');
             C1 = unique(strsplit(lower(name)));
-            semelhantes = cell(1,5); % 5 filmes, no máximo
-            minimos = ones(1,5); % as 5 menores distancias de J
+            distancias = zeros(1, size(movies,1));
             for i=1:size(movies,1)
-                filme = movies{1,1};
+                filme = movies{i,1};
                 C2 = unique(strsplit(lower(filme)));
-                distJ = 1 - (length(intersect(C1,C2))/length(union(C1,C2)))
+                distancias(i) = 1 - (length(intersect(C1,C2))/length(union(C1,C2)));
+                % distancia de Jaccard
+            end
+            [mi,index] = mink(distancias,5); 
+            % as 5 menores distancias e seus indices
+            for i = 1:5
+                if mi(i) > 0.99 % não mostrar se a distancia for > 0.99
+                    if i == 1
+                        disp("We don't have nothing like that :(");
+                    end
+                    break
+                end
+                fprintf(distancias(index(i)) + " | " + movies{index(i),1} + "\n");
             end
     end
     
