05e316e8e75f4335ce5af3762c8f51588d30a219
Lucius Vinicius <luciusviniciusf@ua.pt>
2021-01-24 21:23:37 +0000

    Formatação de comentários para o relatório

--- alt_DJB31MA.m
+++ alt_DJB31MA.m
@@ -5,7 +5,6 @@ function h= alt_DJB31MA(chave, seed, k)
 %  chave    array de caracteres com a chave
 %  seed     semente que permite obter vários hash codes para a mesma chave
 %   k       número de hashfunctions
-%
 %   h        hashcode devolvido
 len= length(chave);
 chave= double(chave);
@@ -16,7 +15,8 @@ end
 
 h = zeros(k,1);
 
-for i=1:5 % evitar de o primeiro valor ser um valor pequeno (5 parece o ideal)
+for i=1:5 
+    % evitar de o primeiro valor ser um valor pequeno (5 parece o ideal)
     val = mod(31 * val(1) + num2str(i), 2^32 -1);
 end
 
--- getShingles.m
+++ getShingles.m
@@ -1,6 +1,7 @@
 function [shingles,n_sh] = getShingles(str,ns)
     N = strlength(str);
-    n_sh = N-ns+1; % number of shingles (sem ignorar diferentes) que vão ter na string
+    n_sh = N-ns+1; 
+    % number of shingles (sem ignorar diferentes) que vão ter na string
     shingles = cell(1,n_sh);
     
     
